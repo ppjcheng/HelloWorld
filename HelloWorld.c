566dc56998db0e5d22607533367419d76564bf35
Jennifer <a-jennch@microsoft.com>
2015-03-09 00:09:23 +0800

    test2 site change 2 files helloworld and file2

--- HelloWorld.c
+++ HelloWorld.c
@@ -7,6 +7,8 @@
 void main()
 
 {
-	
-printf(" hello world ****, make my changes toooooo owhat is going on then\r\n");
+   printf(" hello world ****, make my changes toooooo owhat is going on then\r\n");
+
+   printf(" hello from test2 \r\n");
+
 }
--- file2.c
+++ file2.c
@@ -5,5 +5,6 @@ void function2()
 
 printf("function 2\r\n");
 printf(" more code \r\n");
+printf(" adding more from test2 \r\n");
 
 }
